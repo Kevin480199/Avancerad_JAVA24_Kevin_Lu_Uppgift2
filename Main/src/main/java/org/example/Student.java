9b816d28e4955080efae11cb0cab21f3e1d1e92f
Kevin Lu <ke28lu@gmail.com>
2024-12-02 09:41:51 +0100

    Implemented addStudent option

---
+++ Main/src/main/java/org/example/Student.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Student {
+    private static int id = 1000;
+    private int studentId;
+    private String name;
+    private int grade;
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+        studentId = id;
+        id++;
+    }
+
+    public int getId() {
+        return studentId;
+    }
+}
